c2002cd8eb54a9a6a432e6232327deefd97050e8
Elyes Fakhar <elyes.fakhar@esprit.tn>
2021-07-17 19:16:00 +0100

    cors

--- index.py
+++ index.py
@@ -1,5 +1,16 @@
 from fastapi import FastAPI
 from routes.user import user
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(user)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
